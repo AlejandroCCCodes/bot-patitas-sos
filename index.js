f284f357b64bd6439cb40ef7f38dc2fa08691987
AlejandroCCCodes <110312076+AlejandroCCCodes@users.noreply.github.com>
2026-07-26 01:06:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -50,8 +50,7 @@ const client = new Client({
             '--no-first-run',
             '--no-zygote',
             '--disable-gpu'
-        ],
-        executablePath: '/opt/render/.cache/puppeteer/chrome/linux-146.0.7680.31/chrome-linux64/chrome'
+        ]
     }
 });
 
